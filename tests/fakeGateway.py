f26856d396ca08c7b8c9520e0674a917fd4e5a5d
Daniel Williams <dwilliams@port8080.net>
2014-11-09 00:18:01 -0700

    Tabs suck.

--- tests/fakeGateway.py
+++ tests/fakeGateway.py
@@ -1,4 +1,4 @@
-#!/use/bin/python
+#!/use/bin/env python
 #
 # fakeGateway.py
 # This script fakes out a gateway for testing the arduino code.
@@ -15,34 +15,34 @@ inputBuffer = ""
 
 ### FUNCTIONS ###
 def unlockDoorAccess():
-	# Send the command to unlock the door for access ( short time)
-	pass
+    # Send the command to unlock the door for access ( short time)
+    pass
 
 def unlockDoorPesistent():
-	# Send the command to unlock the door for persistent
-	pass
+    # Send the command to unlock the door for persistent
+    pass
 
 def lockDoor():
-	# Send the command to lock the door
-	pass
+    # Send the command to lock the door
+    pass
 
 ### MAIN ###
 def main():
-	# Open the serial port
-	ser = serial.Serial( serialPort, 9600, timeout = 0)
-	# Loop indefinitely until a CTRL+C
-	while( True):
-		# Append the serial input to an input buffer
-		#   Requests are 16 bytes in size, so try to read 16 bytes
-		inputBuffer.append( ser.read(16))
-		# Check if there's a complete request.  This will probably require some validation.
-		# If the request is good, reply with the proper sequence
-		#   If the first test card is swiped, reply with an unlockDoorAccess
-		#   If the second test card is swiped, replay with an unlockDoorPersistent, then a lockDoor after a delay
-		# Sleep for a second
-		time.sleep(1)
+    # Open the serial port
+    ser = serial.Serial( serialPort, 9600, timeout = 0)
+    # Loop indefinitely until a CTRL+C
+    while( True):
+        # Append the serial input to an input buffer
+        #   Requests are 16 bytes in size, so try to read 16 bytes
+        inputBuffer.append( ser.read(16))
+        # Check if there's a complete request.  This will probably require some validation.
+        # If the request is good, reply with the proper sequence
+        #   If the first test card is swiped, reply with an unlockDoorAccess
+        #   If the second test card is swiped, replay with an unlockDoorPersistent, then a lockDoor after a delay
+        # Sleep for a second
+        time.sleep(1)
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
