2c64466cfd6a74080f1bf3f6996e494acd622e04
Daniel Williams <dwilliams@port8080.net>
2014-11-08 22:56:35 -0700

    Have the test script laid out.  Still need to finish it.

--- tests/fakeGateway.py
+++ tests/fakeGateway.py
@@ -4,6 +4,7 @@
 # This script fakes out a gateway for testing the arduino code.
 
 ### IMPORTS ###
+import time
 import serial
 
 ### GLOBALS ###
@@ -13,6 +14,17 @@ inputBuffer = ""
 ### CLASSES ###
 
 ### FUNCTIONS ###
+def unlockDoorAccess():
+	# Send the command to unlock the door for access ( short time)
+	pass
+
+def unlockDoorPesistent():
+	# Send the command to unlock the door for persistent
+	pass
+
+def lockDoor():
+	# Send the command to lock the door
+	pass
 
 ### MAIN ###
 def main():
@@ -23,21 +35,14 @@ def main():
 		# Append the serial input to an input buffer
 		#   Requests are 16 bytes in size, so try to read 16 bytes
 		inputBuffer.append( ser.read(16))
-		# Check if there's a complete request
+		# Check if there's a complete request.  This will probably require some validation.
 		# If the request is good, reply with the proper sequence
 		#   If the first test card is swiped, reply with an unlockDoorAccess
 		#   If the second test card is swiped, replay with an unlockDoorPersistent, then a lockDoor after a delay
+		# Sleep for a second
+		time.sleep(1)
+
 
 
 if __name__ == '__main__':
 	main()
-
-
-ser = serial.Serial(, 9600, timeout=0)
->>> s = ser.read(100)
->>> s = ser.read(100)
->>> s
-'\x026A0049F63EEB\r\n\x03'
->>> s = ser.read(100)
->>> s
-'\x026A0049DFB844\r\n\x03'
